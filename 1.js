7a46827827d9ab879529d5f401a3cbe47f79812e
JojoHojo <georgiapella@hotmail.com>
2023-05-29 23:52:29 +0300

    final-part1

--- 1.js
+++ 1.js
@@ -1,6 +1,10 @@
+
+
+/*
 var toggle = document.getElementById("toggle");
 var container = document.getElementById("container");
 
 toggle.onclick = function(){
 	container.classList.toggle('active');
 }
+*/
--- game.js
+++ game.js
@@ -1,77 +1,71 @@
-
-/*
 const question = document.getElementById("question");
-const choices = Array.from(document.getElementsByClassName('choice-text'));
-const progressText = document.getElementById('progressText');
-const scoreText = document.getElementById('score');
-//const progressBarFull = document.getElementById('progressBarFull');
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+const questionCounterText = document.getElementById("questionCounter");
+const scoreText = document.getElementById("score");
 
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
-let availableQuestions = [];
-*/
+let availableQuesions = [];
 
-
-
+/* THE QUESTIONS / ANSERS OF THE TEST */
 let questions = [
-    { //1erwtisi
+    { 
         question: "Πόσο χρόνο πρόκειτε να έχετε διαθέσιμο για το κατοικιδιο σας;;", //erwtisi
-        choice1: "Πάρα πολύ",    //apanthseis mc
+        choice1: "Αρκετό",    //apanthseis mc
         choice2: "Ελάχιστο",
-        choice3: "Μερικό",
-        choice4: "Όσο χρειάζεται",
-        answer: 1   //swsth apantish -> ch1 
+        choice3: "Δεν ξέρω σίγουρα",
+        answer: 1   //'swsth' apantish
     },
+    /*
     { 
         question: "Είστε αλλεργικοί στα ζώα ;;",
-        choice1: "Όχι σε κανένα",    
+        choice1: "Όχι",    
         choice2: "Μόνο στους σκύλους",
         choice3: "Μόνο στις γάτες",
         choice4: "Ναι ",
-        answer: 3  
+        answer: 4  
     },
+    */
     { 
         question: "Πως θα περιγράφατε τον εαυτό σας ;;",
-        choice1: "Εσωστρεφής",    
+        choice1: "Εσωστρεφής / Κλειστός",    
         choice2: "Κοινωνικός / Εξωστρεφής",
-        choice3: "Αλλάζω πάντα, δεν ειμαι κατι συγκεκριμένο",
-        choice4: "Ψυχαγωγικος",
-        answer: 4
+        choice3: "Δεν ειμαι κατι συγκεκριμένο",
+        answer: 2 
     },
     {
     question: "Τι μεγεθος θα προτειμούσατε για το κατοικιδιο σας ;;",
-        choice1: "Πολύ μικρό",    
-        choice2: "Τεράστιο",
-        choice3: "Δεν εχω κάποια προτείμηση",
-        choice4: "Μεσαιο",
-        answer: 4
+        choice1: "Μικρό",    
+        choice2: "Μεγάλο",
+        choice3: "Δεν θέλω κάτι συγκεκριμένο",
+        answer: 2
     },
+    /*
     {
         question: "Τι υψος θα προτειμούσατε για το κατοικιδιο σας ;;",
             choice1: "Ψηλό",    
             choice2: "Κοντό",
             choice3: "Δεν εχω κάποια προτείμηση",
-            choice4: "Κανονικο / Νορμαλ",
             answer: 4
-    },
+    },*/
     {
         question: "Μπορείς να ζησεις με καποιο θορυβοδες κατοικιδιο ;",
             choice1: "Δεν ξερω",    
-            choice2: "Ναι",
-            choice3: "Όχι, δεν νομίζω",
-            choice4: "Ανάλογα πόσο συχνά είναι θορυβόδες & πόσο",
-            answer: 4
+            choice2: "Όχι, δεν νομίζω",
+            choice3: "Ναι",
+            choice4: "Ανάλογα",
+            answer: 3
     },
     {
-        question: "Ειστε σε θεση να αλληλεπιδρατε με το ζωο σας καθε μερα;;",
-            choice1: "Όχι",    
+        question: "Ειστε σε θεση να αλληλεπιδρατε με το ζωο σας καθε μερα;",
+            choice1: "Όχι κάθε μέρα ",    
             choice2: "Ναι φυσικά",
             choice3: "Ετσι και ετσι, ανάλογα τη ημερα",
-            choice4: "Πολύ λίγο",
-            answer: 4
-    },
+            choice4: "Δεν ξέρω",
+            answer: 2
+    }, /*
     {
         question: "Τι θα θελατε να κανετε με το κατοικιδιο σας πιο πολυ ;;",
             choice1: "Να του μαθω κολπα / εκπαιδευσω",    
@@ -79,129 +73,165 @@ let questions = [
             choice3: "Μονο Παρέα / Συντροφιά",
             choice4: "Όλα",
             answer: 4
-    },
+    },*/
     {
-        question: "Γιατί θέλεις ενα κατοικίδιο ;;",
-            choice1: "Για φυλακα / προστασια",    
+        question: "Γιατί θέλεις ενα κατοικίδιο ;",
+            choice1: "Για προστασια / βοήθεια κάποιου προβληματος",    
             choice2: "Παρεα / συντροφια",
             choice3: "Έτσι θα ηθελα απλα",
-            choice4: "Για βοήθεια λόγο κάποιου προβλήματος",
-            answer: 4
+            answer: 1
     },
     {
-        question: "Λειπεις συχνα για ωρες απο το σπίτι ;;",
+        question: "Λειπεις συχνα για ωρες απο το σπίτι ;",
             choice1: "Εξαρτατε την ημερα",    
-            choice2: "Οχι ",
-            choice3: "Ναι αρκετές ωρες",
-            choice4: "Ετσι και ετσι ",
-            answer: 4
+            choice2: "Ναι αρκετές ωρες",
+            choice3: "Οχι ",
+            answer: 3
     },
     {
         question: "Πόσο χώρο έχεις για το κατοικιδιο σου ;;",
-            choice1: "Μικρο",    
-            choice2: "Πάρα πολύ",
+            choice1: "Μικρο / Λίγο",    
+            choice2: "Αρκετό",
             choice3: "Ετσι και ετσι",
-            choice4: "Καθόλου",
-            answer: 4
+            choice4: "Σχεδόν Καθόλου",
+            answer: 2
     },
     {
-        question: "Σας αρέσουν τα κλασσικα κατοικιδια (σκύλος / γάτα) ή τα λίγο πιο διαφορετικά ;;",
-            choice1: "Τα κλασσικά",    
-            choice2: "Τα διαφορετικά",
-            choice3: "Δεν έχω θέμα",
-            choice4: "Ανάλογα όχι κατι πολύ εξτριμ",
-            answer: 4
-        },
+      question: "Πόσο θα σας ενοχλούσε αν γυρνούσατε σπίτι και βλέπατε κάποια πραγματα σας χαλασμένα;",
+          choice1: "Πολύ",    
+          choice2: "Καθόλου σιγά / Ακόμη και να με πείραζε δεν θα θύμωνα με το ζωάκι μου! ",
+          choice3: "Ανάλογα, πάντως γενικά δεν μαρέσει αυτό! ",
+          choice4: "Δεν ξέρω",
+          answer: 2
+  },
+  {
+    question: "Θα θέλατε κάποιο προστατευτικό κατοικίδιο;",
+        choice1: "Μπα ",    
+        choice2: "Δεν ξέρω.. δεν νομίζω ",
+        choice3: "Ναι!",
+        answer: 3
+  },
+  {
+    question: "Θα θέλατε το κατοικιδιο σας να έχει κάποια προσωπικότητα / χαρητομενια ;",
+        choice1: "Ναι πολύ ",    
+        choice2: "Όχι θα με κούραζε",
+        choice3: "Δεν είναι επικύνδιο ;!",
+        answer: 1
+  },
+  {
+    question: "Θα θέλατε το κατοικιδιο σας να είναι με προσωπικότητα πολύ διακτριτικη ;",
+        choice1: "Δεν ξέρω",    
+        choice2: "Ναι ακριβώς!  ",
+        choice3: "Γενικά όχι δεν έχω θέμα και να μη είναι ",
+        answer: 3
+  },
+  {
+    question: "Θα θέλατε ένα φίλο για αγκαλιές;",
+        choice1: "Ναι",    
+        choice2: "Όχι",
+        choice3: "Ουτε καν, συχαινομαι ",
+        choice4: "Δεν ξέρω",
+        answer: 1
+  },
+  {
+    question: "Θα πήγενες βόλτα με το ζωάκι σου;", 
+        choice1: "Όχι",
+        choice2: "Ναι",   
+        choice3: "Δεν ξέρω",
+        answer: 2
+  },
+  {
+    question: "Κατοικιδιο και μπάνιο; Πως σας φαίνεται", 
+        choice1: "Όχι βαριεμαι / Δεν θέλω / Δεν χρειάζεται",
+        choice2: "Ναι φυσικά! ",   
+        choice3: "Εξαρταται",
+        answer: 2
+  },
+  {
+    question: "Κατοικιδιο υπερβολικά δραστηριο και ενεργητικο", 
+        choice1: "Ναι ναι ναι ",
+        choice2: "απαπαπα ",   
+        answer: 1
+  },
+  {
+    question: "Θέλετε ένα μικρό ζώο ή ένα που μπορεί ενδεχομένως να γίνει μεγαλύτερο, αναλόγος ;", 
+        choice1: "Όχι μικρό είναι οκ  ",
+        choice2: "Δεν έχω θέμα ",   
+        answer: 2
+  },
+  {
+    question: "Περιπέτειες με το κατοικιδιο σας, πως θα σας φαινοταν ;", 
+        choice1: "Ναι γιατι όχι / Αμα βολέψει βεβαίος  ",
+        choice2: "Δεν ξερω / Δεν νομίζω ",
+        choice3: "Μπα το θέλω σπιτι ",
+        answer: 2
+  }
 
-
-
-
+  
 ];
 
-
-/*
-fetch("questions.json").then(res => {
-    console.log(res);
-})
-
-
-
 //CONSTANTS
-const COREECT_BONUS = 10;
-const MAX_QUESTIONS = 3;
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 5;
 
 startGame = () => {
-    questionCounter = 0;
-    score = 0;
-    availableQuestions = [...questions];
-    console.log(availableQuestions);
-    getNewQuestion();
+  questionCounter = 0;
+  score = 0;
+  availableQuesions = [...questions];
+  getNewQuestion();
 };
 
 getNewQuestion = () => {
+  if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    //go to the end page
+    if(score >= 30)
+        return window.location.assign("./dog.html");
+    else 
+        return window.location.assign("./cat.html");
+  }
+  questionCounter++;
+  questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
 
-        if(availableQuestions.length == 0 || questionCounter >= MAX_QUESTIONS){
-            //go to the end page
-            return window.location.assign("./end.html");
-        }
-    questionCounter++;
-    progressText.innerText = 'Question' + questionCounter + "/" + MAX_QUESTIONS;  //emf -> to 1/3 klp
-                                //or ... = '${questionCounter}/${MAX_QUESTIONS}';  its the same 
-    
-    //Update the progress bar
-    // progressBarFull.style.width = '${(questionCounter / MAX_QUESTIONS) * 100}%';
+  const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+  currentQuestion = availableQuesions[questionIndex];
+  question.innerText = currentQuestion.question;
 
-    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionIndex];
-    question.innerText = currentQuestion.question;
+  choices.forEach(choice => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
 
-    //pianw tis epiloges 
-    choices.forEach( choice => {
-        const number = choice.dataset['number'];
-        choice.innerText = currentQuestion['choice' + number];
-    });
-
-    availableQuestions.splice(questionIndex, 1); //
-
-    acceptingAnswers = true;
+  availableQuesions.splice(questionIndex, 1);
+  acceptingAnswers = true;
 };
 
-//Allagh erwthewn kai epilogh apanthsewn.. 
 choices.forEach(choice => {
-    choice.addEventListener('click',e => {
-        if(!acceptingAnswers) return;
+  choice.addEventListener("click", e => {
+    if (!acceptingAnswers) return;
 
-        acceptingAnswers = false;
-        const selectedChoice = e.target;
-        const selectedAnswers = selectedChoice.dataset['number'];
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
 
-        const classToApply = selectedAnswers == currentQuestion.answer ? "correct" : "incorrect";
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
 
+    if (classToApply === "correct") {
+      incrementScore(CORRECT_BONUS);
+    }
 
-        if(classToApply == 'correct'){
-            incrementScore(CORRECT_BONUS);
-        }
+    selectedChoice.parentElement.classList.add(classToApply);
 
-        getNewQuestion();
-    });
-
-})
-
-//oso afora to score: 
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
+  });
+});
 
 incrementScore = num => {
-    score +=num;
-    scoreText.innerText = score;
+  score += num;
+  scoreText.innerText = score;
 };
 
-
 startGame();
-
-
-
-
-
-
-
-
-*/
